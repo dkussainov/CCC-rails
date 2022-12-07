575299b1685ed79152e41339adfebe9cb0a097d2
Alexis Torosina <alexistorosina1@gmail.com>
2022-12-07 18:38:40 -0500

    set default likes to 0

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,7 @@
 class LikesController < ApplicationController
+    def update
+        user = User.find_by(id: session[:user_id])
+        post = user.posts.find(params[:post_id])
+        likes = post.likes
+
 end
